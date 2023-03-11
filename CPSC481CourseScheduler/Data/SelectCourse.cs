24f613afa946f8d883ae4bb26225075c87f4e8d1
wycmichelle11 <79016649+wycmichelle11@users.noreply.github.com>
2023-03-11 07:58:36 -0700

    Shows location map, just need to change title

--- CPSC481CourseScheduler/Data/SelectCourse.cs
+++ CPSC481CourseScheduler/Data/SelectCourse.cs
@@ -8,9 +8,8 @@
         public string InstructorName { get; set; }
         public string CourseTime { get; set; }
         public string Seats { get; set; }
-
         public string Days { get; set; }
-
         public string LectureNumber { get; set; }
+        public string Map { get; set; }
     }
 }
